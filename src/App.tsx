13116cf1de41fdd619e161e0a88ec5f095972603
gambasz <guidoambasz@gmail.com>
2024-07-20 13:00:18 -0400

    Added style changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <div className="Title">
-        <h1>Catan Board Generator!</h1>
+        <h1>Catan Board Generator</h1>
       </div>
       <div className="Board">
         <div className="view-container">
@@ -20,12 +20,17 @@ function App() {
         </div>
       </div>
       <div className="Generation">
-        <button type="button" onClick={handleClick}>
-          Click Me
+        <button type="button" className="generate-button" role="button" onClick={handleClick}>
+          New Board
         </button>
       </div>
       <div className="Footer">
-        Made by me, blah bla.
+        <p>
+          Made by <a href="https://github.com/gambasz">Guido Ambasz</a>
+        </p>
+        <p>
+          You can find the source code for it on <a href="https://github.com/gambasz/catan-board-gen">Github</a>
+        </p>
       </div>
     </div>
   );
