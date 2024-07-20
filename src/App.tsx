5a203d4a9fb98cb9499f879aa6448b52994e4eef
gambasz <guidoambasz@gmail.com>
2024-07-19 22:10:57 -0400

    Add base HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Catan Board Generator!</h1>
+      <div className="Title">
+        <h1>Catan Board Generator!</h1>
+      </div>
+      <div className="Board">
+        The board goes here
+      </div>
+      <div className="Generation">
+        Here goes the button
+      </div>
+      <div className="Footer">
+        Made by me, blah bla.
+      </div>
     </div>
   );
 }
